7f724ec41e2e5fb496a1e090e719ef631b1da759
franchux <franchux@2ff15a2c-12e3-11de-a46f-cd5bcf99231e>
2009-03-23 12:58:00 +0000

    empezado a programar el controlador y la vista

--- 03_Agenda/src/es/random/agenda/negocio/GestorAgenda.java
+++ 03_Agenda/src/es/random/agenda/negocio/GestorAgenda.java
@@ -17,4 +17,80 @@ import es.random.agenda.datos.Contacto;
 public class GestorAgenda {
     Contacto[] contactos;
 
+    public GestorAgenda(){
+        contactos = new Contacto[0]; // Inicializamos el vector
+    }
+
+    /**
+     * Añade un contacto al vector
+     * @param nuevoContacto datos del nuevo contacto a incluir
+     */
+    public void AñadirContacto(Contacto nuevoContacto) {
+        // Necesitamos ampliar el tamaño de nuestro vector, así que creamos uno
+        // del tamaño que necesitamos.
+        Contacto[] nuevaLista = new Contacto[contactos.length + 1];
+
+        // Si ya hay contactos anteriores debemos copiarlos primero
+        if(contactos.length > 0) {
+            for(int i = 0; i < contactos.length; i++) {
+                nuevaLista[i] = contactos[i];
+            }
+        }
+        // En el último añadimos el nuevo
+        nuevaLista[nuevaLista.length - 1] = nuevoContacto;
+
+        // Sobreescribimos el vector antiguo con la nueva
+        contactos = nuevaLista;
+    }
+
+    
+
+    /**
+     * Borra un contacto del vector
+     *
+     * @param indice número de contacto a borrar
+     */
+    public void borrarContacto(int indice) {
+        // La lista tendrá ahora un contacto menos
+        Contacto[] nuevaLista = new Contacto[contactos.length - 1];
+
+        int y = 0;
+        for(int x = 0; x < contactos.length - 1; x++) {
+            if(x != indice) {
+                nuevaLista[y++] = contactos[x];
+            }
+        }
+        contactos = nuevaLista;
+    }
+
+    /**
+     * Borra un contacto
+     *
+     * @param aEliminar objeto de tipo Contacto
+     */
+
+    public void borrarContacto(Contacto aEliminar) {
+        // Buscar indice
+        // Llamar a borrar por ind
+    }
+
+
+    /**
+     * Borra un contacto
+     *
+     * Parametros variables. Puede recibir:
+     *  - nombre
+     *  - nombre + apellido
+     *  - nombre + apellido + apellido
+     *
+     * @param nombre nombre a borrar
+     */
+    public void borrarContacto(String[] nombre) {
+        // Crear nuevo objeto
+        // Llamar a borrar pasando el contacto
+    }
+    
+    public int buscarIndiceContacto(Contacto aBuscar) {
+        // TODO -
+    }
 }
---
+++ 03_Agenda/src/es/random/agenda/ui/InterfazConsolaAgenda.java
@@ -0,0 +1,32 @@
+/*
+ * Vista "Modo Consola" para la aplicacion
+ */
+
+package es.random.agenda.ui;
+
+/**
+ *
+ * @author: $Author$
+ * @version: $Rev$
+ * @date: $Date$
+ * $Id$
+ */
+public class InterfazConsolaAgenda {
+
+    /**
+     * Constructor de la clase
+     *
+     * - Muestra el menu
+     * - Captura pulsacion
+     * - Ejecuta la tarea correspondiente
+     *
+     */
+    public InterfazConsolaAgenda() {
+        boolean continuar = true;
+        do {
+            // mostrarMenu()
+            // seleccionarOpcion()
+            // ejecutarTarea()
+        } while(continuar);
+    }
+}
