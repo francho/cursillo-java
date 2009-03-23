a3b6be97b16085e6465dbb52d555eebfe5f968e3
franchux <franchux@2ff15a2c-12e3-11de-a46f-cd5bcf99231e>
2009-03-23 12:01:00 +0000

    Prueba de svn:keywords

---
+++ 03_Agenda/src/es/random/agenda/datos/PruebaSVN.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.random.agenda.datos;
+
+/**
+ *
+ *
+ * @author: $Author$
+ * @version: $Rev$
+ * @Fecha: $Date$
+ */
+public class PruebaSVN {
+
+}
